48de5571a80930d1d53a092c7f5bb1b8cfec2fe0
Xuncheng Wang <w.xun.cheng@gmail.com>
2013-12-20 15:57:08 +0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+commendies = Category.create(name: 'TV Commedies')
+dramas = Category.create(name: 'TV Dramas')
+reality = Category.create(name: 'Reality TV')
+
+Video.create(title: "Family Guy", description: "Peter griffin and talking dog", small_cover_url: "/tmp/family_guy.jpg", category: commendies)
+Video.create(title: "Futurama", description: "Space travel!", small_cover_url: "/tmp/futurama.jpg", category: dramas)
+Video.create(title: "Monk", description: "Paranoid SF detective", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: dramas)
+Video.create(title: "South Park", description: "Hippie kides", small_cover_url: "/tmp/south_park.jpg", category: reality)
+
