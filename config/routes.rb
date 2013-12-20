44b987d36e0621526c19594c71d0f22566d571fd
Xuncheng Wang <w.xun.cheng@gmail.com>
2013-12-20 23:45:13 +0800

    add the front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Netflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
+  root to: "pages#front"
   get "home", to: "videos#index"
+
   resources :categories, only: [:show]
   resources :videos, only: [:show] do
     post :search, on: :collection
