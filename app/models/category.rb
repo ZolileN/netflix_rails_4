f97503871b6e2ca7c9efc6ad55bb67d375692709
Xuncheng Wang <w.xun.cheng@gmail.com>
2013-12-20 23:30:25 +0800

    show only recent videos in the homepage

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,9 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
-  has_many :videos
+  has_many :videos, -> { order "created_at DESC" }
+
+  def recent_videos
+    videos.limit(6)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,38 @@ describe Category do
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
   it { should have_many(:videos) }
+
+  describe "#recent_videos" do
+    it "returns videos in a descending order for created_at" do
+      comedies = Category.create!(name: "Comedies")
+      family_guy = Video.create!(title: "Family Guy", created_at: 1.day.ago, description: "Talking dog", category: comedies)
+      south_park = Video.create!(title: "South Park", description: "Hippy Kids", category: comedies)
+      expect(comedies.recent_videos).to eq([south_park, family_guy])
+    end
+
+    it "returns all videos if there are less than 6 videos" do
+      comedies = Category.create!(name: "Comedies")
+      family_guy = Video.create!(title: "Family Guy", description: "Talking dog", category: comedies)
+      south_park = Video.create!(title: "South Park", description: "Hippy Kids", category: comedies)
+      expect(comedies.recent_videos.size).to eq(2)
+    end
+
+    it "returns 6 videos if there are more than 6 videos" do
+      comedies = Category.create!(name: "Comedies")
+      7.times { Video.create!(title: "foo", description: "bar", category: comedies) }
+      expect(comedies.recent_videos.size).to eq(6)
+    end
+
+    it "returns the latest 6 videos" do
+      comedies = Category.create!(name: "Comedies")
+      family_guy = Video.create!(title: "Family Guy", created_at: 1.day.ago, description: "Talking dog", category: comedies)
+      6.times { Video.create!(title: "foo", description: "bar", category: comedies) }
+      expect(comedies.recent_videos).not_to include(family_guy)
+    end
+
+    it "returns an empty array if there are no videos" do
+      comedies = Category.create!(name: "Comedies")
+      expect(comedies.recent_videos).to eq([])
+    end
+  end
 end
