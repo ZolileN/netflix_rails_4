c7ffb24afc6a41b6498422efd770ea8c342163a0
Xuncheng Wang <w.xun.cheng@gmail.com>
2013-12-20 15:45:18 +0800

    add video model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131220073634) do
+ActiveRecord::Schema.define(version: 20131220074057) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "videos", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "small_cover_url"
+    t.string   "large_cover_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
