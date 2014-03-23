d1fc5c84318fcce5f153a6c0082e9eee32dd6cc4
Xuncheng Wang <w.xun.cheng@gmail.com>
2014-03-23 11:33:01 +0800

    fix specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :reviews
-  has_many :queue_items, -> { order :position }
+  has_many :queue_items, -> { order('position') }
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
 
   def normalize_queue_item_positions
     queue_items.each_with_index do |queue_item, index|
-      queue_item.update(:position, index + 1)
+      queue_item.update(position: index + 1)
     end
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
   # it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:full_name) }
   it { should have_many(:reviews) }
-  it { should have_many(:queue_items).order(:position) }
+  it { should have_many(:queue_items).order('position') }
   it { should have_many(:relationships).with_foreign_key("follower_id").dependent(:destroy) }
   it { should have_many(:followed_users).through(:relationships) }
   it { should have_many(:reverse_relationships).with_foreign_key("followed_id").dependent(:destroy) }
